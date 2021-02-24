bc848b133fe990345d6b06d741cfb964748cfb60
vancampioni <vanessacrtsilva@gmail.com>
2021-02-24 13:17:26 +0000

    (23/02) Término da revisão de JS

--- revisao-js/parametros-predefinidos.js
+++ revisao-js/parametros-predefinidos.js
@@ -1,6 +1,10 @@
 //Parâmetros predefinidos ou opcionais em funções
+//Regras:
+//1) Parâmetros opcionais devem vir após os obrigatórios
+//2) Pode haver mais de um parâmetro opcional por função
 
-function calcularArea(base, altura, forma, casasDec = 2) {
+//forma como parâmetro predefinido ou opcional
+function calcularArea(base, altura, forma = 'Q', casasDec = 2) {
     let res
     switch(forma) {
         case 'Q': //Quadrilátero
@@ -17,8 +21,8 @@ function calcularArea(base, altura, forma, casasDec = 2) {
 
 console.log(calcularArea(20, 8, 'Q'))
 console.log(calcularArea(7.5, 3, 'T'))
-console.log(calcularArea(4.5, 4.5, 'E'))
-console.log(calcularArea(30,60))
+console.log(calcularArea(4.5, 4.5, 'E', 4))
+console.log(calcularArea(30, 60))
 //Caso seja necessário omitir um parâmetro predefinido que não
 //seja o último, pode-se usar o valor undefined em seu lugar
 console.log(calcularArea(30, 60, undefined, 3))
